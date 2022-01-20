0413a5eac293200976353169d346a89455264c50
mrparkersson <amoparkerprince78@gmail.com>
2022-01-20 10:06:12 +0000

    Add delete function and check button to update the dom

--- src/class.test.js
+++ src/class.test.js
@@ -1,6 +1,29 @@
 const Tasks = require('./class');
+const updateTask = require('./update');
 
 const tasks = new Tasks();
+document.body.innerHTML = `<main>
+<div class="head">
+  <h2>Today's To Do</h2>
+  <span class="material-icons">autorenew</span>
+</div>
+<div class="table">
+  <form class="add">
+    <input
+      class="input"
+      id="input"
+      type="text"
+      placeholder="Add to your list"
+      required
+    />
+    <button type="submit" id="add">
+      <span class="material-icons">keyboard_return</span>
+    </button>
+  </form>
+  <ul class="list"></ul>
+</div>
+<div class="clear">Clear all completed</div>
+</main>`;
 
 describe('testing add', () => {
   test('adding the first task', () => {
@@ -49,6 +72,24 @@ describe('testing clearing completed', () => {
   tasks.edit(currentTask);
   test('The list should be empty after clearing', () => {
     tasks.clearCompleted();
+    expect(tasks.list.length).toBe(0);
+  });
+});
+
+describe('testing dom manipulation functions', () => {
+  localStorage.clear();
+  const tasks = new Tasks();
+  test('An li element should appear when adding', () => {
+    tasks.add({ description: 'A task was added' });
+    updateTask(tasks);
+    expect(document.querySelectorAll('li').length).toBe(1);
+  });
+  test('Complete should be true after we click checkbox', () => {
+    document.querySelector('.check').click();
+    expect(tasks.list[0].completed).toBeTruthy();
+  });
+  test('Deleting an item should delete in the dom', () => {
+    document.querySelector('.dele').click();
     expect(tasks.list.length).toBe(0);
   });
 });
--- src/index.js
+++ src/index.js
@@ -1,98 +1,18 @@
 import 'material-icons/iconfont/material-icons.css';
 import './style.css';
 import Tasks from './class';
+import updateTask from './update';
 
-const listParent = document.querySelector('.list');
 const clearAll = document.querySelector('.clear');
 const form = document.querySelector('form');
 
 const tasks = new Tasks();
 
-const updateTask = () => {
-  listParent.innerHTML = '';
-
-  tasks.list
-    .sort((a, b) => a.index - b.index)
-    .forEach((t) => {
-      listParent.innerHTML += `
-      <li id="task-${t.index}" draggable="true">
-        <div class="content">
-          <input class="check" type="checkbox" ${t.completed ? 'checked' : ''}/>
-          <input class="input" type="text" value='${t.description}' readonly />
-        </div>
-        <div class="actions">
-          <span class="material-icons drag">more_vert</span>
-          <span class="material-icons dele">delete_outline</span>
-        </div>
-      </li>
-      `;
-    });
-
-  const lis = document.querySelectorAll('li');
-  lis.forEach((li) => {
-    li.addEventListener('click', (e) => {
-      const elm = e.target;
-      if (elm.classList.contains('drag') || elm.classList.contains('check')) {
-        return;
-      }
-
-      lis.forEach((elm) => elm.classList.remove('active'));
-      li.classList.add('active');
-
-      const inp = li.querySelector('.input');
-      inp.readOnly = false;
-      inp.focus();
-      inp.setSelectionRange(inp.value.length, inp.value.length);
-    });
-  });
-
-  document.querySelectorAll('li .input').forEach((inp) => {
-    inp.addEventListener('focusout', () => {
-      setTimeout(() => {
-        inp.parentNode.parentNode.classList.remove('active');
-
-        inp.readOnly = true;
-      }, 200);
-    });
-    inp.addEventListener('input', () => {
-      const id = Number(inp.parentNode.parentNode.id.split('-')[1]);
-
-      const obj = tasks.list.find((t) => t.index === id);
-
-      obj.description = inp.value.trim();
-
-      tasks.edit(obj);
-    });
-  });
-
-  document.querySelectorAll('li .check').forEach((inp) => {
-    inp.addEventListener('change', () => {
-      const id = Number(inp.parentNode.parentNode.id.split('-')[1]);
-
-      const obj = tasks.list.find((t) => t.index === id);
-
-      obj.completed = inp.checked;
-
-      tasks.edit(obj);
-    });
-  });
-
-  document.querySelectorAll('.dele').forEach((delBtn) => {
-    delBtn.addEventListener('click', () => {
-      const id = Number(delBtn.parentNode.parentNode.id.split('-')[1]);
-
-      tasks.remove(id);
-
-      delBtn.parentNode.parentNode.remove();
-    });
-  });
-};
-
-updateTask();
+updateTask(tasks);
 
 clearAll.addEventListener('click', () => {
   tasks.clearCompleted();
-  updateTask();
+  updateTask(tasks);
 });
 
 form.addEventListener('submit', (e) => {
@@ -104,5 +24,5 @@ form.addEventListener('submit', (e) => {
 
   form.reset();
 
-  updateTask();
+  updateTask(tasks);
 });
