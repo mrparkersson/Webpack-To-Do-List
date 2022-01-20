7a9b8f7abf1d6c7986ff5fd09bb878bc65a6f2f9
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-01-20 10:57:58 +0000

    Add tests to add and remove function

--- src/class.test.js
+++ src/class.test.js
@@ -17,6 +17,13 @@ describe('testing add', () => {
     tasks.add({ description: 'task 3' });
     expect(tasks.list[2].description).toBe('task 3');
   });
+
+  test('Adding several tasks', () => {
+    tasks.add({ description: 'task 3' });
+    tasks.add({ description: 'task 4' });
+    tasks.add({ description: 'task 5' });
+    expect(tasks.list.length).toBe(6);
+  });
 });
 
 describe('testing delete', () => {
@@ -24,6 +31,19 @@ describe('testing delete', () => {
 
   test('test if first element is deleted', () => {
     tasks.remove(removeIndex);
-    expect(tasks.list.length).toBe(2);
+    expect(tasks.list.length).toBe(5);
+  });
+
+  test('test if another element is deleted', () => {
+    tasks.remove(removeIndex);
+    expect(tasks.list.length).toBe(4);
+  });
+
+  test('test removing several elements', () => {
+    tasks.remove(removeIndex);
+    tasks.remove(removeIndex);
+    tasks.remove(removeIndex);
+    tasks.remove(removeIndex);
+    expect(tasks.list.length).toBe(0);
   });
 });
